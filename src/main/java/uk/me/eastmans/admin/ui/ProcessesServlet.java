a9a85e41320de7009ecb1308c3a7879711be3c05
markeastman <meastman@redhat.com>
2016-09-28 10:46:27 +0100

    Added fragmets and showed how the left menu can be used to provide high level navigation

---
+++ src/main/java/uk/me/eastmans/admin/ui/ProcessesServlet.java
@@ -0,0 +1,28 @@
+package uk.me.eastmans.admin.ui;
+
+import javax.inject.Inject;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by markeastman on 26/09/2016.
+ */
+@WebServlet(value="/processes")
+public class ProcessesServlet extends HttpServlet {
+    @Inject
+    private HtmlProducer uiProducer;
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+        Map model = new HashMap();
+        model.put( "username", "Mark Eastman" );
+        uiProducer.processRequest(request,response, "processes", request.getLocale(), model);
+    }
+}
